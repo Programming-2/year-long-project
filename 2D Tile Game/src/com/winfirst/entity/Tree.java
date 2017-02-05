b9bf34524ec0c12efdc991bef9f53ee4fbb5eff9
squirlemaster42 <squirlemaster@gmail.com>
2017-02-05 18:06:42 -0500

    Formatting

--- 2D Tile Game/src/com/winfirst/entity/Tree.java
+++ 2D Tile Game/src/com/winfirst/entity/Tree.java
@@ -24,7 +24,7 @@ public class Tree extends StaticEntity{
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.tree, (int) (x - handler.getGameCamera().getxOffset()) , (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+		g.drawImage(Assets.tree, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
 		
 //		g.setColor(Color.red);
 //		g.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffset()),
