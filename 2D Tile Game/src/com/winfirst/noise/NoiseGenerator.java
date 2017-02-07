58da77896d93f0d12f4f1b9f4bb888cf2e289e30
squirlemaster42 <squirlemaster@gmail.com>
2017-02-07 17:17:25 -0500

    Started noise

---
+++ 2D Tile Game/src/com/winfirst/noise/NoiseGenerator.java
@@ -0,0 +1,15 @@
+package com.winfirst.noise;
+
+import java.util.ArrayList;
+
+public class NoiseGenerator {
+	private float scale;
+	
+	public NoiseGenerator(float scale){
+		this.scale = scale;
+	}
+	
+	public float generateNoise(){
+		return 0.0f;
+	}
+}
