1cf28428d35834674cea55eeb48c4ebef0a330c4
squirlemaster42 <squirlemaster@gmail.com>
2017-04-25 21:13:37 -0400

    Config

--- 2D Tile Game/src/com/winfirst/config/Config.java
+++ 2D Tile Game/src/com/winfirst/config/Config.java
@@ -3,6 +3,17 @@ package com.winfirst.config;
 public class Config <T>{
 
     private T t;
+    private String name;
 
+    private T value;
+
+    public Config(String name){
+        this.name = name;
+        readConfig();
+    }
+
+    private void readConfig(){
+
+    }
 
 }
--- 2D Tile Game/src/com/winfirst/config/ConfigFile.java
+++ 2D Tile Game/src/com/winfirst/config/ConfigFile.java
@@ -1,4 +1,29 @@
 package com.winfirst.config;
 
+import com.winfirst.logging.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class ConfigFile {
+
+    private String file;
+
+    private ArrayList<String> fileAsList;
+
+    public ConfigFile(String file){
+        this.file = file;
+        this.fileAsList = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+            fileAsList.add(br.readLine());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Logger.getInstance().write("Configs have crashed");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Logger.getInstance().write("Configs have crashed");
+        }
+    }
 }
