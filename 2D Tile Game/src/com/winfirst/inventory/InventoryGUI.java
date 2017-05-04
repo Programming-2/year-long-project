15f67a1d325a360b1894a94046864ea39b9ac39c
Jack Fireblade <balancejackfireblade@gmail.com>
2017-05-04 10:20:38 -0400

    Inventory GUI Creation

---
+++ 2D Tile Game/src/com/winfirst/inventory/InventoryGUI.java
@@ -0,0 +1,21 @@
+package com.winfirst.inventory;
+
+import com.winfirst.entity.Player;
+
+public class InventoryGUI {
+
+    private Inventory inventory;
+    private Player player;
+
+    public InventoryGUI(Inventory inventory, Player player){
+        this.inventory = inventory;
+        this.player = player;
+    }
+
+    public Inventory getInventory(){
+        return inventory;
+    }
+
+    
+
+}
