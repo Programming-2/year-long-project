1e6bf3d651d47743f9fec008d86088bf04df42a7
Jakob Misbach <squirlemaster@gmail.com>
2017-05-22 10:06:08 -0400

    Kinda works

--- 2D Tile Game/src/com/winfirst/projectile/Bullet.java
+++ 2D Tile Game/src/com/winfirst/projectile/Bullet.java
@@ -18,78 +18,9 @@ public class Bullet extends Projectile{
 
     @Override
     public void tick() {
-        move();
-    }
-
-    public void move() {
-        if (!checkEntityCollision(super.getVector().getXVol(), 0f)) {
-            moveX();
-        }
-        if (!checkEntityCollision(0f, super.getVector().getYVol())) {
-            moveY();
-        }
-    }
-
-    public void moveX() {
-        if (super.getVector().getXVol() > 0) {//Moving right
-            int tx = (int) (x + super.getVector().getXVol() + bounds.x + bounds.width) / Tile.TILEWIDTH;
-
-            if (!collisionWithTile(tx, (int) (y + bounds.y) / Tile.TILEHEIGHT) && !collisionWithTile(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT)) {
-                super.setX((float) (((super.getX() + super.getVector().getXVol()))));
-            } else {
-                super.getHandler().getEntityManager().removeEntity(this);
-            }
-
-        } else if (super.getVector().getXVol() < 0) {//Moving left
-            int tx = (int) (x + super.getVector().getXVol() + bounds.x) / Tile.TILEWIDTH;
-
-            if (!collisionWithTile(tx, (int) (y + bounds.y) / Tile.TILEHEIGHT) && !collisionWithTile(tx, (int) (y + bounds.y + bounds.height) / Tile.TILEHEIGHT)) {
-                super.setX((float) (((super.getX() + super.getVector().getXVol()))));
-            } else {
-                super.getHandler().getEntityManager().removeEntity(this);
-            }
-        }
-    }
-
-    public void moveY() {
-        if (super.getVector().getYVol() < 0) {//Up
-            int ty = (int) (y + super.getVector().getYVol() + bounds.y) / Tile.TILEHEIGHT;
-
-            if (!collisionWithTile((int) (x + bounds.x) / Tile.TILEWIDTH, ty) && !collisionWithTile((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty)) {
-                super.setY((float) (((super.getY() + super.getVector().getYVol()))));
-            } else {
-                super.getHandler().getEntityManager().removeEntity(this);
-            }
-
-        } else if (super.getVector().getYVol() > 0) {//Down
-            int ty = (int) (y + super.getVector().getYVol() + bounds.y + bounds.height) / Tile.TILEHEIGHT;
-
-            if (!collisionWithTile((int) (x + bounds.x) / Tile.TILEWIDTH, ty) && !collisionWithTile((int) (x + bounds.x + bounds.width) / Tile.TILEWIDTH, ty)) {
-                super.setY((float) (((super.getY() + super.getVector().getYVol()))));
-            } else {
-                super.getHandler().getEntityManager().removeEntity(this);
-            }
-        }
-    }
-
-    protected boolean collisionWithTile(int x, int y) {
-        return handler.getWorld().getTile(x, y).isSolid();
-    }
-
-    @Override
-    public boolean checkEntityCollision(float xOffset, float yOffset) {
-        for (Entity e : handler.getWorld().getEntityManager().getEntities()) {
-            if (e.equals(this) || e instanceof Bullet) {
-                continue;
-            } else if(e.equals(super.getHandler().getEntityManager().getPlayer())) {
-                continue;
-            }
-            if (e.getCollisionBounds(0, 0).intersects(getCollisionBounds(xOffset, yOffset))) {
-                return true;
-            }
-        }
-
-        return false;
+        super.getVector().tick();
+        super.setX(super.getVector().getX());
+        super.setY(super.getVector().getY());
     }
 
     @Override
