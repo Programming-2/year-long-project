ac7596d88055a7e42625dd2f092106c5e69fd3f6
Jakob Misbach <jakobmisbach8@gmail.com>
2017-11-11 21:03:49 -0500

    Organization

--- 2D Tile Game/src/com/winfirst/main/Launcher.java
+++ 2D Tile Game/src/com/winfirst/main/Launcher.java
@@ -7,13 +7,13 @@ import com.winfirst.logging.Logger;
 public class Launcher {
 
     public static void main(String[] args) {
-        //		SettingsGUI settings = new SettingsGUI(300, 300);
-        //		settings.makeSettingsGui();
+        //SettingsGUI settings = new SettingsGUI(300, 300);
+        //settings.makeSettingsGui();
 
         //Logger Init
-        Logger.setPath("res/log/");
-        Logger.getInstance().write("Path set");
-        ConfigFile cfgconfigfilefile = new ConfigFile("res/config/config.cfg");
+        //Logger.setPath("res/log/");
+        //Logger.getInstance().write("Path set");
+        //ConfigFile cfgconfigfilefile = new ConfigFile("res/config/config.cfg");
 
         //Starts Game
         Game game = new Game("RPG Game", 1280, 720);
