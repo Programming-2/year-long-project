a4a9587db81815f6b69653f8a57c88bbd6d5da6c
Jakob Misbach <jakobmisbach8@gmail.com>
2017-11-21 08:39:58 -0500

    Added basic equations

--- 2D Tile Game/src/com/winfirst/physics/KinematicUtils.java
+++ 2D Tile Game/src/com/winfirst/physics/KinematicUtils.java
@@ -2,5 +2,15 @@ package com.winfirst.physics;
 
 public class KinematicUtils {
 
-    
+    public static float vel(float vNaught, float a, float t){
+        return a * t + vNaught;
+    }
+
+    public static float distance(float xNaught, float vNaught, float a, float t){
+        return (float) ((.5 * a * t * t) + (vNaught * t) + xNaught);
+    }
+
+    public static float vel(float vNaught, float a, float xNaught, float x){
+        return (float) (Math.sqrt(Math.pow(vNaught, 2) + 2 * a * (x - xNaught)));
+    }
 }
